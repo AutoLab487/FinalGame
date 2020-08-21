6cfb0691945a89a75cbf4040cd7cfbb5f3cd1290
399abdulsamad <56542466+399abdulsamad@users.noreply.github.com>
2020-08-21 22:09:30 +0500

    AbdulSamadVersion

---
+++ grid1.0/Assets/Scripts/Answer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Answer: MonoBehaviour
+{
+    public string value, value1;
+    public InputField inputField, inputField1, inputField2, inputField3;
+    public GameObject display_text;
+    private bool correct,incorrect, correct1, incorrect1;
+    public GameObject GameWinpopUp, GameLosepopUp;
+
+    public void check()
+    {
+        if(inputField.text=="A")
+        {
+            correct = true;
+        }
+        else
+        {
+            incorrect = true;
+        }
+        value = inputField.text;
+        //display_text = inputField.GetComponent<Text>().text = "this " + value + " this is input";
+
+        if (inputField1.text == "A")
+        {
+            correct1 = true;
+        }
+        else
+        {
+            incorrect1 = true;
+        }
+        value1 = inputField1.text;
+
+        //validate();
+    }
+
+  
+    public void validate()
+    {   if (correct == true && correct1 == true)//(correct && correct1  && correct2 && correct3)
+        {
+            //Debug.Log("asdas");
+            GameWinpopUp.SetActive(true);
+
+        }
+        //value = inputField.text;
+        //display_text = inputField.GetComponent<Text>().text = "this " + value + " this is input";
+        else
+        {
+            GameLosepopUp.SetActive(true);
+        }
+
+    }
+    
+
+
+}
---
+++ grid1.0/Assets/Scripts/Menue_Controller.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menue_Controller : MonoBehaviour
+{
+    public void MainMen2()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    /// <summary>
+    /// This is for DFA game
+    /// </summary>
+
+    public void DFALevel1()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void DFALevel2()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void DFALevel3()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void DFALevel4()
+    {
+        SceneManager.LoadScene(5);
+    }
+    /// <summary>
+    /// this is for Regx Game level
+    /// </summary>
+    public void RELevel1()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void RELevel2()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void RELevel3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
