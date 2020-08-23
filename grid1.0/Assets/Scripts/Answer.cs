3fd0ce9e3a25d9cb4ea5bb927003db47bd66d1e8
399abdulsamad <56542466+399abdulsamad@users.noreply.github.com>
2020-08-23 15:55:09 +0500

    samadChanges23Augest

--- grid1.0/Assets/Scripts/Answer.cs
+++ grid1.0/Assets/Scripts/Answer.cs
@@ -6,20 +6,50 @@ using UnityEngine.UI;
 public class Answer: MonoBehaviour
 {
     public string value, value1;
-    public InputField inputField, inputField1, inputField2, inputField3;
+    public InputField inputField, inputField1, inputField2, inputField3, inputField4,inputField5;
     public GameObject display_text;
-    private bool correct,incorrect, correct1, incorrect1;
+    private bool correct, correct1, correct2, correct3, correct4;
     public GameObject GameWinpopUp, GameLosepopUp;
 
-    public void check()
+    public void check1()
     {
-        if(inputField.text=="A")
+        if (inputField4.text == "0")
+        {
+            correct4 = true;
+        }
+    }
+    public void Validate1()
+    {
+        if (correct4)
+        {
+        GameWinpopUp.SetActive(true);
+        }
+        else
+        {
+            GameLosepopUp.SetActive(true);
+        }
+    }
+    public void check2()
+    {
+
+    }
+
+
+
+
+
+
+
+
+    public void check3()
+    {
+        if (inputField.text == "A")
         {
             correct = true;
         }
         else
         {
-            incorrect = true;
+            correct = false;
         }
         value = inputField.text;
         //display_text = inputField.GetComponent<Text>().text = "this " + value + " this is input";
@@ -30,16 +60,36 @@ public class Answer: MonoBehaviour
         }
         else
         {
-            incorrect1 = true;
+            correct1 = false;
         }
         value1 = inputField1.text;
 
+        if (inputField2.text == "B")
+        {
+            correct2 = true;
+        }
+        else
+        {
+            correct2 = false;
+        }
+        value1 = inputField2.text;
+
+        if (inputField3.text == "B")
+        {
+            correct3 = true;
+        }
+        else
+        {
+            correct3 = false;
+        }
+        value1 = inputField2.text;
         //validate();
+
     }
 
-  
-    public void validate()
-    {   if (correct == true && correct1 == true)//(correct && correct1  && correct2 && correct3)
+
+    public void validate3()
+    {   if (correct == true && correct1==true && correct2==true && correct3==true)//(correct && correct1  && correct2 && correct3)
         {
             //Debug.Log("asdas");
             GameWinpopUp.SetActive(true);
